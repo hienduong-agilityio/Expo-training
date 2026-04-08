244b6ad1c2017b268f0ca27f5fce497f4a22ab3a
Hien The Duong <hien.duong@asnet.com.vn>
2026-04-08 15:02:08 +0700

    feat: migrate from react native cli to expo cli

--- android/app/src/main/java/com/reactnativetemplate/MainApplication.kt
+++ android/app/src/main/java/com/reactnativetemplate/MainApplication.kt
@@ -15,7 +15,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
+      ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -28,10 +28,10 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
+      })
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+    get() = ExpoReactHostFactory.getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
